1d9364838c50ed86c84287633179c584beae916a
Rik-149 <fredpieterse34@gmail.com>
2025-05-29 15:14:37 +0200

    added boot and app scripts

---
+++ USER/AdditionalRadioStations.js
@@ -0,0 +1,150 @@
+// =============================================================================
+//  Name: Additional Radio Stations
+//  License: CC-BY-NC-4.0
+//  Repository: https://github.com/CodyTolene/pip-apps
+//  Description: Adds additional radio stations to the Pip-Boy 3000 mkV. Specifically,
+//               Appalachia Radio, Diamond City Radio, and Radio New Vegas.
+//  Version: 1.0.0
+// =============================================================================
+
+const fs = require('fs');
+const COLOR_GREEN = '#0F0';
+const COLOR_WHITE = '#FFF';
+const HEIGHT = g.getHeight();
+const WIDTH = g.getWidth();
+const CENTER = WIDTH / 2;
+let cache = { enabled: false };
+let build_dir = { enabled: false };
+
+function loadAndCreateRadioStations(filePath, baseDir) {
+  let radioStations = {
+    list: [],
+    byFolder: {},
+    byType: {},
+  };
+  filePath = filePath || 'USER/AdditionalRadioStations/radioStations.json';
+  baseDir = baseDir || 'USER/AdditionalRadioStations';
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const stations = JSON.parse(data);
+    radioStations.list = stations;
+    radioStations.byFolder = {};
+    radioStations.byType = {};
+    stations.forEach(function (station) {
+      radioStations.byFolder[station.folder] = station;
+      if (!radioStations.byType[station.type]) {
+        radioStations.byType[station.type] = [];
+      }
+      radioStations.byType[station.type].push(station);
+    });
+    stations.forEach(function (station) {
+      const stationPath = baseDir + '/' + station.folder;
+
+      try {
+        fs.mkdirSync(stationPath);
+      } catch (e) {
+        if (e.code === 'EEXIST') {
+          console.log('Folder already exists:', stationPath);
+        } else {
+          console.error(e.message);
+        }
+      }
+    });
+  } catch (err) {
+    console.error('Error loadAndCreateRadioStations():', err.message);
+  }
+}
+
+function buildAndSaveClipCache() {
+  let folders = ['KPSS', 'APPALACHIA', 'DIAMOND_CITY', 'NEW_VEGAS'];
+  let types = Object.values(CLIP_TYPE);
+  let cache = {};
+  folders.forEach((folder) => {
+    let folderCache = {};
+    try {
+      process.memory();
+      let allFiles = fs
+        .readdirSync('USER/AdditionalRadioStations/' + folder)
+        .sort();
+      types.forEach((type) => {
+        folderCache[type] = allFiles
+          .filter(
+            (f) =>
+              f.startsWith(type) &&
+              f.toUpperCase().endsWith('WAV') &&
+              !f.startsWith('.'),
+          )
+          .map((f) => `USER/AdditionalRadioStations/${folder}/${f}`);
+      });
+    } catch (e) {
+      folderCache = {};
+    }
+    cache[folder] = folderCache;
+  });
+  process.memory();
+  fs.writeFile(
+    'USER/AdditionalRadioStations/clipCache.json',
+    JSON.stringify(cache),
+  );
+  return cache;
+}
+
+function draw() {
+  g.clear();
+
+  g.setFontMonofonto28();
+  g.setColor(COLOR_GREEN);
+  g.drawString('Additional Radio Stations', CENTER, 30);
+
+  g.setFontMonofonto18();
+  g.setColor(COLOR_WHITE);
+  g.drawString('Options Menu', CENTER, 55);
+
+  g.drawString(
+    '[ ' + (cache.enabled ? 'X' : ' ') + ' ] Build Clip Cache',
+    CENTER,
+    HEIGHT * 0.3,
+  );
+
+  g.drawString(
+    '[ ' + (build_dir.enabled ? 'X' : ' ') + ' ] Build Directories from JSON',
+    CENTER,
+    HEIGHT * 0.6,
+  );
+
+  g.drawString('Press radio wheel to create directories', CENTER, HEIGHT - 80);
+  g.drawString('Press wheel in to build cache.', CENTER, HEIGHT - 60);
+  g.drawString('Press torch to exit.', CENTER, HEIGHT - 35);
+}
+
+function toggleCache(dir) {
+  cache.enabled = !cache.enabled;
+  buildAndSaveClipCache();
+  draw();
+}
+
+Pip.on('knob2', function (dir) {
+  build_dir.enabled = !build_dir.enabled;
+  loadAndCreateRadioStations();
+  draw();
+});
+
+function handleTorch() {
+  E.reboot();
+}
+
+draw();
+
+Pip.on('knob1', function (dir) {
+  Pip.knob1Click();
+
+  if (dir < 0) draw();
+  else if (dir > 0) draw();
+  else toggleCache();
+});
+
+Pip.on('torch', handleTorch);
+
+// problems: boot app is too big, so this app runs out of memory
+// todo: only load at boottime when this app has run
+// todo: make UI with button to make folders and button to generate clipcache
---
+++ USER_BOOT/AdditionalRadioStations.js
@@ -0,0 +1,314 @@
+// =============================================================================
+//  Name: Additional Radio Stations
+//  License: CC-BY-NC-4.0
+//  Description: Adds additional radio stations to the Pip-Boy 3000 mkV. Specifically,
+//               Appalachia Radio, Diamond City Radio, and Radio New Vegas.
+//  Version: 1.0.0
+// =============================================================================
+
+const DEBUG = false;
+fs = require('fs');
+
+function debug() {
+  if (!DEBUG) return;
+  print.apply(null, arguments);
+}
+
+function getCachedClipList(folder, type) {
+  if (!cachedRadioClips[folder]) cachedRadioClips[folder] = {};
+  if (!cachedRadioClips[folder][type]) {
+    try {
+      let path = `USER/AdditionalRadioStations/${folder}`;
+      let allFiles = require('fs').readdirSync(path).sort();
+      cachedRadioClips[folder][type] = allFiles.filter(
+        (f) =>
+          f.startsWith(type) &&
+          f.toUpperCase().endsWith('WAV') &&
+          !f.startsWith('.'),
+      );
+    } catch (e) {
+      cachedRadioClips[folder][type] = []; // fallback to empty list
+    }
+  }
+  return cachedRadioClips[folder][type];
+}
+
+let radioPlayClipCustom = (a, station, b) => (
+  a == undefined && (a = CLIP_TYPE.MUSIC),
+  new Promise((e, f) => {
+    var c = null;
+    let d = () => {
+      Pip.removeListener('streamStopped', d),
+        (Pip.radioClipPlaying = !1),
+        c && rd.setVol(c),
+        e(1);
+    };
+    if (Pip.radioClipPlaying)
+      Pip.removeListener('streamStopped', d),
+        Pip.videoStop(),
+        (Pip.radioClipPlaying = !1),
+        c && rd.setVol(c),
+        e(0);
+    if (!Pip.radioClipPlaying) {
+      (c = rd.getVol()),
+        rd.setVol(2),
+        a == CLIP_TYPE.ANY &&
+          (a = [CLIP_TYPE.MUSIC, CLIP_TYPE.VOICE, CLIP_TYPE.SFX][
+            Math.floor(Math.random() * 2.999)
+          ]);
+      let e = getCachedClipList(station, a);
+      e.length || f('No radio clips found');
+      let g = getRandomExcluding(e.length, Pip.lastClipIndex);
+      b && console.log(`Playing radio clip type ${a}: ${e[g]}`),
+        console.log(e[g]),
+        Pip.audioStart(e[g]),
+        Pip.on('streamStopped', d),
+        (Pip.radioClipPlaying = !0),
+        (Pip.lastClipIndex = g);
+    }
+  })
+);
+
+try {
+  cachedRadioClips = JSON.parse(
+    require('fs').readFileSync('USER/AdditionalRadioStations/clipCache.json'),
+  );
+} catch (e) {
+  // buildAndSaveClipCache();
+}
+
+(function initAdditionalRadiostations() {
+  debug('[initAdditionalRadiostations] Initializing custom radio patch');
+
+  try {
+    // Ensure MODE and MODEINFO are defined
+    if (
+      typeof MODEINFO !== 'undefined' &&
+      typeof MODE !== 'undefined' &&
+      MODEINFO[MODE.RADIO]
+    ) {
+      const radioTab = MODEINFO[MODE.RADIO];
+
+      // OPTIONAL: remove the default function if present
+      if (radioTab.fn) {
+        delete radioTab.fn;
+      }
+
+      // Define your own radio behavior
+      radioTab.fn = function () {
+        debug('[radioTab.fn] Custom radio logic triggered');
+        // Clear screen and do your thing
+        bC.clear(1);
+        bC.setColor(3)
+          .setFontMonofonto16()
+          .drawString('Custom Radio Active', 40, 40);
+        bC.flip();
+
+        // Insert your radio logic here — play audio, update display, etc.
+        rd._options || rd.setupI2C(), bC.clear(1);
+        let f = 0;
+        let cachedRadioClips = {};
+        let a = Graphics.createArrayBuffer(120, 120, 2, {
+          msb: !0,
+        });
+        E.getAddressOf(a, 0) == 0 &&
+          ((a = undefined),
+          E.defrag(),
+          (a = Graphics.createArrayBuffer(120, 120, 2, {
+            msb: !0,
+          })));
+        let c = new Uint16Array(60);
+        for (let l = 0; l < 60; l += 2) c[l] = l * 2;
+
+        function j() {
+          for (let a = 0; a < 40; a++) {
+            let c = 2,
+              b = 1;
+            a % 5 == 0 && ((c = 3), (b = 2)),
+              bC.setColor(c),
+              bC.drawLine(245 + a * 3, 143 - b, 245 + a * 3, 143),
+              bC.drawLine(367 - b, 22 + a * 3, 367, 22 + a * 3);
+          }
+          bC.setColor(3)
+            .drawLine(245, 144, 367, 144)
+            .drawLine(368, 144, 368, 22)
+            .flip();
+        }
+
+        function k() {
+          if ((a.clearRect(0, 0, 119, 119), Pip.radioClipPlaying))
+            Pip.getAudioWaveform(c, 20, 100);
+          else if (Pip.radioOn)
+            for (let a = 1; a < 60; a += 2)
+              c[a] = E.clip(
+                60 + (analogRead(RADIO_AUDIO) - 0.263) * 600,
+                0,
+                119,
+              );
+          else {
+            let a = f;
+            for (let b = 1; b < 60; b += 2)
+              c[b] = 60 + Math.sin(a) * 45 * Math.sin((a += 0.6) * 0.13);
+          }
+          a.drawPolyAA(c),
+            (f += 0.3),
+            Pip.blitImage(a, 285, 85, {
+              noScanEffect: !0,
+            });
+        }
+        E.showMenu({
+          '': {
+            x2: 240,
+            predraw: function () {
+              bC.drawImage(a, 245, 20), rd.drawFreq(bC);
+            },
+          },
+          'FM Radio': {
+            value: rd.isOn(),
+            format: (a) => (a ? 'On' : 'Off'),
+            onchange: (a) => {
+              a
+                ? ((Pip.radioKPSS = !1),
+                  rd.enable(!0),
+                  Pip.audioStart('UI/RADIO_ON.wav'))
+                : (rd.enable(!1),
+                  rd.drawFreq(),
+                  Pip.audioStart('UI/RADIO_OFF.wav'));
+            },
+          },
+          'FM Volume': {
+            value: rd.getVol(),
+            min: 0,
+            max: 15,
+            step: 1,
+            onchange: (a) => {
+              rd.setVol(a);
+            },
+          },
+          'KPSS Radio': {
+            value: !!Pip.radioKPSS,
+            format: (a) => (a ? 'On' : 'Off'),
+            onchange: (a) => {
+              (Pip.radioKPSS = a),
+                a
+                  ? radioPlayClipCustom(CLIP_TYPE.ANY, 'KPSS')
+                  : Pip.audioStart('UI/RADIO_OFF.wav');
+            },
+          },
+          'Appalachia Radio': {
+            value: !!Pip.radioAPPA,
+            format: (a) => (a ? 'On' : 'Off'),
+            onchange: (a) => {
+              (Pip.radioAPPA = a),
+                a
+                  ? radioPlayClipCustom(CLIP_TYPE.ANY, 'APPALACHIA')
+                  : Pip.audioStart('UI/RADIO_OFF.wav');
+            },
+          },
+          'Diamond City Radio': {
+            value: !!Pip.radioDIAM,
+            format: (a) => (a ? 'On' : 'Off'),
+            onchange: (a) => {
+              (Pip.radioDIAM = a),
+                a
+                  ? radioPlayClipCustom(CLIP_TYPE.ANY, 'DIAMOND_CITY')
+                  : Pip.audioStart('UI/RADIO_OFF.wav');
+            },
+          },
+          'Radio New Vegas': {
+            value: !!Pip.radioVEGA,
+            format: (a) => (a ? 'On' : 'Off'),
+            onchange: (a) => {
+              (Pip.radioVEGA = a),
+                a
+                  ? radioPlayClipCustom(CLIP_TYPE.ANY, 'NEW_VEGAS')
+                  : Pip.audioStart('UI/RADIO_OFF.wav');
+            },
+          },
+        });
+        let g = Pip.removeSubmenu;
+        j();
+        let h = setInterval(() => {
+          Pip.radioKPSS && !Pip.streamPlaying()
+            ? radioPlayClipCustom(CLIP_TYPE.ANY, 'KPSS')
+            : k();
+          Pip.radioAPPA && !Pip.streamPlaying()
+            ? radioPlayClipCustom(CLIP_TYPE.ANY, 'APPALACHIA')
+            : k();
+          Pip.radioDIAM && !Pip.streamPlaying()
+            ? radioPlayClipCustom(CLIP_TYPE.ANY, 'DIAMOND_CITY')
+            : k();
+          Pip.radioVEGA && !Pip.streamPlaying()
+            ? radioPlayClipCustom(CLIP_TYPE.ANY, 'NEW_VEGAS')
+            : k();
+        }, 50);
+        (rd.rdsTimer = setInterval(() => {
+          readRDSData();
+        }, 100)),
+          rd.isOn() && (rd.getChannelInfo(), rd.drawFreq());
+        let b = null;
+        let e = 0;
+        let d = null;
+
+        function i(a) {
+          if (
+            Pip.radioKPSS ||
+            Pip.radioAPPA ||
+            Pip.radioDIAM ||
+            Pip.radioVEGA
+          ) {
+            if (Pip.radioKPSS) radioPlayClipCustom(CLIP_TYPE.MUSIC, 'KPSS');
+            if (Pip.radioAPPA)
+              radioPlayClipCustom(CLIP_TYPE.MUSIC, 'APPALACHIA');
+            if (Pip.radioDIAM)
+              radioPlayClipCustom(CLIP_TYPE.MUSIC, 'DIAMOND_CITY');
+            if (Pip.radioVEGA)
+              radioPlayClipCustom(CLIP_TYPE.MUSIC, 'NEW_VEGAS');
+            return;
+          }
+          // FM radio tuning logic (only runs if no custom station is on)
+          if (!d && a == e) {
+            rd.freq = rd.freq + e * 0.1;
+            rd.freq < rd.start / 100 && (rd.freq = rd.end / 100);
+            rd.freq > rd.end / 100 && (rd.freq = rd.start / 100);
+            rd.drawFreq();
+            b && clearTimeout(b);
+            b = setTimeout(() => {
+              try {
+                rd.freqSet(rd.freq);
+              } catch (err) {
+                log(`Error tuning radio: ${err}`);
+              }
+              b = null;
+            }, 200);
+            d && clearTimeout(d);
+            d = setTimeout(() => (d = null), 20);
+          } else {
+            e = a;
+          }
+        }
+        Pip.on('knob2', i);
+        Pip.removeSubmenu = function () {
+          (Pip.radioKPSS = !1),
+            (Pip.radioAPPA = !1),
+            (Pip.radioDIAM = !1),
+            (Pip.radioVEGA = !1),
+            clearInterval(h),
+            rd.tuningInterval && clearInterval(rd.tuningInterval),
+            (rd.tuningInterval = null),
+            rd.rdsTimer && clearInterval(rd.rdsTimer),
+            (rd.rdsTimer = null),
+            Pip.removeListener('knob2', i),
+            b && clearTimeout(b),
+            g();
+        };
+      };
+
+      debug('[initAdditionalRadiostations] Radio tab overridden');
+    } else {
+      debug('[initAdditionalRadiostations] Radio mode not available');
+    }
+  } catch (err) {
+    print('[initAdditionalRadiostations] Failed to patch radio tab:', err);
+  }
+})();
